fe99f92cb8ab4fd03949d5cf0e2c019b21e9a042
Christopher Borchert <christopher.borchert@gmail.com>
2016-03-25 06:01:43 -0400

    Configure footer output

--- footer.php
+++ footer.php
@@ -1,13 +1,20 @@
         
                 </div> <!-- /.row -->
             </div> <!-- /.container -->
-            <?php if ( is_active_sidebar( 'sidebar-foot' ) ) : ?>
-            <div class="col-xs-10 sidebar-foot-container">
-                <ul id="sidebar-foot">
-                    <?php dynamic_sidebar( 'sidebar-foot' ); ?>
-                </ul>
-            </div>
-            <?php endif; ?>
+            <footer class="col-xs-12">
+                <div class="row">
+                    <?php if ( is_active_sidebar( 'sidebar-foot' ) ) : ?>
+                        <div class="col-xs-12 col-sm-9 sidebar-foot-container">
+                            <ul id="sidebar-foot">
+                                <?php dynamic_sidebar( 'sidebar-foot' ); ?>
+                            </ul>
+                        </div>
+                    <?php endif; ?>
+                    <div class="col-xs-12 col-sm-3 copyright text-right">
+                        <p>Copyright &copy; <?php echo date('Y');?> Q2Q Comics</p>
+                    </div>
+                </div>
+            </footer>
         </div><!-- /#the_page -->
         <?php wp_footer(); ?>
     </body>
