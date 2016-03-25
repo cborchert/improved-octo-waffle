4445f52b4dbac0c2ad10b45842d1f683daafbd5b
Christopher Borchert <christopher.borchert@gmail.com>
2016-03-25 07:18:58 -0400

    Up to date

--- footer.php
+++ footer.php
@@ -4,14 +4,14 @@
             <footer class="col-xs-12">
                 <div class="row">
                     <?php if ( is_active_sidebar( 'sidebar-foot' ) ) : ?>
-                        <div class="col-xs-12 col-sm-9 sidebar-foot-container">
+                        <div class="col-xs-12 col-sm-6 col-md-9 sidebar-foot-container">
                             <ul id="sidebar-foot" class="sidebar">
                                 <?php dynamic_sidebar( 'sidebar-foot' ); ?>
                             </ul>
                         </div>
                     <?php endif; ?>
-                    <div class="col-xs-12 col-sm-3 copyright text-right">
-                        <p>Copyright &copy; <?php echo date('Y');?> Q2Q Comics</p>
+                    <div class="col-xs-12 col-sm-6 col-md-3 copyright">
+                       <span>Copyright &copy; <?php echo date('Y');?> Q2Q Comics</span>
                     </div>
                 </div>
             </footer>
