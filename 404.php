dbf498f15c48508be201979a30957e6faff04bb0
Chris Borchert <cborchertrun@gmail.com>
2016-04-21 17:52:18 +0200

    Add 404 page

---
+++ 404.php
@@ -0,0 +1,19 @@
+<?php get_header(); //sets up a container and row ?>
+
+<div class='col-xs-12 col-xs-push-0 <?php echo (is_active_sidebar( 'sidebar-main' ))?'col-md-9 ':'col-md-12'; ?> col-md-push-0 main-content' >
+
+    <div class="search-header">
+        <h1> Nothing but darkness. </h1>
+    </div>
+    <div class="search-page-search-form">
+        <?php
+            echo get_search_form();
+        ?>
+    </div>
+    
+</div>
+
+<?php get_template_part('sidebar', 'down'); ?>
+
+
+<?php get_footer(); //closes a container and a row //make sure no white space after tag ?>
