5f7039a76ddf624b46134864a6986a684f63605c
Chris Borchert <cborchertrun@gmail.com>
2017-03-01 10:56:48 +0100

    Small tweaks

--- page.php
+++ page.php
@@ -5,7 +5,7 @@
         if( have_posts() ): 
             while( have_posts()): //start the loop
                 the_post();
-                get_template_part('post', 'content');
+                get_template_part('page', 'content');
             endwhile; 
     
         endif; //end the loop
